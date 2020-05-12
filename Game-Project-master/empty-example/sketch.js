9ec6256220273edec4469a010e321c2d23f27de1
blueshrimp123 <61802995+blueshrimp123@users.noreply.github.com>
2020-05-11 23:17:40 -0700

    Terrain Beta as of 5/13/20 - Blank Screen

--- Game-Project-master/empty-example/sketch.js
+++ Game-Project-master/empty-example/sketch.js
@@ -1,16 +1,92 @@
 let noiseScale=0.02;
+let b
+
+function setup() {
+  createCanvas(800, 600);
+  b = line
+
 
 function draw() {
-  createCanvas(800,600)
-background(0,200,230);
-  for (let x=0; x < width; x++) {
-    let noiseVal = noise((x)*noiseScale/2, 80*noiseScale);
-    stroke(235);
-    line(this.x, noiseVal*200+200, x, height);
+  background(0,200,230);
 
-    this.move = function() {
-      this.x = this.x += 5 ;
-      this.y = this.y + random(-1, 1);
+  b.moveline
 
-      if(this.x >= width){
-      this.x = 1;
+
+  for (let x = 0; x < width; x++) {
+  let noiseVal = noise((x)*noiseScale/2, 80*noiseScale);
+
+  stroke(235);
+  line(x, noiseVal*200+200, x, height);
+
+  class line {
+
+    moveline(){
+      this.x = this.x+2;
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let noiseScale=0.02;
+    //
+    // function draw() {
+    //   createCanvas(800,600)
+    // background(0,200,230);
+    //
+    //
+    //
+    // for (let x = 0; x < width; x++) {
+    //     let noiseVal = noise((x)*noiseScale/2, 80*noiseScale);
+    //
+    //     stroke(235);
+    //     line(x, noiseVal*200+200, x, height);
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //     }
+    //
+    //
+    //
+    //
+    //
+    //
+    //     }
