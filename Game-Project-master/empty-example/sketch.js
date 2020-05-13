9c651dd8230539774f1b55f60e2cae5e84609e85
blueshrimp123 <61802995+blueshrimp123@users.noreply.github.com>
2020-05-13 11:45:40 -0700

    MOVING TERRAIN

--- Game-Project-master/empty-example/sketch.js
+++ Game-Project-master/empty-example/sketch.js
@@ -1,12 +1,46 @@
-function setup() {
-  createCanvas(700, 600);
+let snowscape = [];
+
+function setup(){
+    createCanvas(800,400)
 }
 
-function draw(){
-	background(220,100,3);
-  fill("black")
-  ellipse(100,100,40,50);
-  fill("blue")
-  rect(200,200,100,150)
+
+function draw() {
+
+  background(0,200,230);
+
+  let drift = new Snow(width,frameCount);
+  snowscape.push(drift);
+
+  for (let i = 0; i<snowscape.length; i++){
+    snowscape[i].drawSnow();
+    snowscape[i].moveSnow();
+  }
+
+  if (snowscape.length>width){
+    snowscape.shift();
+  }
+
+
+}
+
+class Snow{
+  constructor(x,startx){
+    this.x=x;
+    this.startx = startx;
+  }
+
+  drawSnow(){
+    let noiseScale=0.02;
+    let noiseVal = noise((this.startx)*noiseScale/2, 80*noiseScale);
+    stroke(235);
+    line(this.x, noiseVal*200+200, this.x, height);
+
+  }
+
+  moveSnow(){
+    this.x = this.x -1;
+
+  }
 
 }
