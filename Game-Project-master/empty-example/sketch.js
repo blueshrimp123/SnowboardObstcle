b0ddfce55e94d6ce5ec4f5e73088ad414caa4f81
blueshrimp123 <61802995+blueshrimp123@users.noreply.github.com>
2020-05-18 09:19:15 -0700

    Finalized moving terrain setup
    Sun, clouds, and Snow terrain are merged. 5/18/2020

--- Game-Project-master/empty-example/sketch.js
+++ Game-Project-master/empty-example/sketch.js
@@ -1,30 +1,78 @@
 let snowscape = [];
+var bubbles = [];
 
-function setup(){
-    createCanvas(800,400)
-}
+
+function setup() {
+  createCanvas(800, 500);
+  for (var i = 0; i < 5; i++) {
+    bubbles[i] = new Bubble();
+    }
+  }
 
 
 function draw() {
-
   background(0,200,230);
+  for (var i = 1; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].display();
 
-  let drift = new Snow(width,frameCount);
-  snowscape.push(drift);
+    let drift = new Snow(width,frameCount);
+    snowscape.push(drift);
 
-  for (let i = 0; i<snowscape.length; i++){
-    snowscape[i].drawSnow();
-    snowscape[i].moveSnow();
-  }
+    for (let i = 0; i<snowscape.length; i++){
+      snowscape[i].drawSnow();
+      snowscape[i].moveSnow();
+    }
 
-  if (snowscape.length>width){
-    snowscape.shift();
+    if (snowscape.length>width){
+      snowscape.shift();
+    }
+
+
+    fill(255,255,51)
+    ellipse(750,50,60,60)
+
+
+
   }
 
 
 }
 
-class Snow{
+function Bubble(){
+  this.x = random(1, width);
+  this.y = random(1, height);
+
+  this.display = function() {
+
+    stroke(255);
+     strokeWeight(1);
+     fill(255)
+    ellipse(this.x, this.y, 24, 27);
+    ellipse(this.x+10,this.y+10,24,24);
+    ellipse(this.x+30,this.y+10,24,24);
+    ellipse(this.x+30,this.y-10,24,24);
+    ellipse(this.x+20,this.y-10,24,24);
+    ellipse(this.x+40,this.y,24,24);
+
+
+
+
+  }
+
+  this.move = function() {
+    this.x = this.x += 4 ;
+    this.y = this.y + random(-1, 1);
+
+    if(this.x >= width){
+    this.x = -1;
+
+
+
+    }
+  }
+}
+  class Snow{
   constructor(x,startx){
     this.x=x;
     this.startx = startx;
